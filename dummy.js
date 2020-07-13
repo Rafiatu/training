a304eeeb466985e6ad70ec19912c4cc3d4d6640a
Rafiatu <61936161+Rafiatu@users.noreply.github.com>
2020-07-13 19:56:58 +0100

    Adding dummy file

---
+++ dummy.js
@@ -0,0 +1,7 @@
+let gad = "Good and Bad "
+let b = 4
+let h = `${b}`
+let yam = [3,6,9]
+let newArr = [...yam, 5, 7]
+// console.log(`${yam.filter(i=> i%2==0)} is divisible by`)
+console.log(newArr)
